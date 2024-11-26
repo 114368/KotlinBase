18f1b35be7cbb87bdb0dc615686b0579ed61483e
jiehe <jiehe@example.com>
2024-11-26 22:11:00 +0800

    base yufa

---
+++ app/src/main/java/com/example/kotlinbase/Base/Demo.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinbase.Base
+
+fun main(){
+    /*val range = 0..10
+    for (i in range){
+        println(i)
+    }*/
+
+    /*for(i in 0 until 10 step 2){
+        println(i)
+    }*/
+
+    for(i in 10 downTo 1){
+        println(i)
+    }
+}
+
+
+class Demo {
+
+
+}
