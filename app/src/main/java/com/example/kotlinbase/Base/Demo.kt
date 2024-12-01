e04f49d03079f01085cc835ceaf0b91aade766fc
jiehe <jiehe@example.com>
2024-12-01 14:52:59 +0800

    base yufa

--- app/src/main/java/com/example/kotlinbase/Base/Demo.kt
+++ app/src/main/java/com/example/kotlinbase/Base/Demo.kt
@@ -10,11 +10,35 @@ fun main(){
         println(i)
     }*/
 
-    for(i in 10 downTo 1){
+    /*for(i in 10 downTo 1){
         println(i)
-    }
+    }*/
+    /*val list = mutableListOf("Apple", "Banana", "Orange", "Pear", "Grape", "Watermelon")
+    val newList = list.map { it.uppercase() }
+    for ( fruit in newList){
+        println(fruit)
+    }*/
+    /*val list = mutableListOf("Apple", "Banana", "Orange", "Pear", "Grape", "Watermelon")
+    val newList = list.filter { it.length <= 5 }
+    for ( fruit in newList){
+        println(fruit)
+    }*/
+    /*val list = mutableListOf("Apple", "Banana", "Orange", "Pear", "Grape", "Watermelon")
+    val anyResult = list.any { it.length<= 5 }
+    val allResult = list.all { it.length <= 5 }
+    println(" anyResult:"+anyResult + " allResult:"+allResult)*/
+    /*Thread(Runnable {
+
+    }).start()*/
+    /*Thread{
+
+    }.start()*/
+    printParams(12,str2 =  "sss")
 }
 
+fun printParams(num: Int = 10, str: String = "aaa", str2: String){
+    println("num= $num, str= $str, str2 = $str2")
+}
 
 class Demo {
 
