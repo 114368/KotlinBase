fd6bebe07744b0a4addcbc45ca29944a7b7ef3d6
jiehe <jiehe@example.com>
2025-01-11 12:03:11 +0800

    base yufa

--- app/src/main/java/com/example/kotlinbase/base/Demo.kt
+++ app/src/main/java/com/example/kotlinbase/base/Demo.kt
@@ -1,6 +1,6 @@
 package com.example.kotlinbase.base
 
-fun main(){
+fun main(args: Array<String>){
     /*val range = 0..10
     for (i in range){
         println(i)
@@ -45,7 +45,7 @@ fun main(){
         toString()
     }
     println(result)*/
-    val list = listOf("Apple", "Banana", "Orange", "Pear", "Grape", "Watermelon")
+    /*val list = listOf("Apple", "Banana", "Orange", "Pear", "Grape", "Watermelon")
     val result = StringBuilder().run{
         append("Start eat fruits.\n")
         for(fruit in list){
@@ -53,8 +53,22 @@ fun main(){
         }
         append("Ate all fruits.")
         toString()
-    }
-    println(result)
+    }*/
+    //println(10.0f/3)
+    /*val numbers = intArrayOf(1, 2, 3)
+    val oceans = listOf("Atlantic", "Pacific")
+    val oddList = listOf(numbers, oceans, "salmon")*/
+    //println("hello ${args[0]}")
+    val a = arrayOf(1, 2, 3)
+    val list = asList(-1, 0, *a, 4)
+    println(list.toList())
+}
+
+fun <T> asList(vararg ts: T): List<T> {
+    val result = ArrayList<T>()
+    for (t in ts) // ts is an Array
+        result.add(t)
+    return result
 }
 
 fun printParams(num: Int = 10, str: String = "aaa", str2: String){
